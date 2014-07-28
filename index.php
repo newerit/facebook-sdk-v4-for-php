5ad9277ac1fe4f5ca55bbaa5707a8c4708891f36
Sohaib Ilyas <roomiprogrammer@gmail.com>
2014-07-28 08:39:29 +0500

    Update index.php
    This file is updated as Facebook updated their SDK for PHP ;)

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 session_start();
 
-require_once( 'Facebook/FacebookHttpable.php' );
-require_once( 'Facebook/FacebookCurl.php' );
-require_once( 'Facebook/FacebookCurlHttpClient.php' );
+require_once( 'Facebook/HttpClients/FacebookHttpable.php' );
+require_once( 'Facebook/HttpClients/FacebookCurl.php' );
+require_once( 'Facebook/HttpClients/FacebookCurlHttpClient.php' );
+
+require_once( 'Facebook/Entities/AccessToken.php' );
+require_once( 'Facebook/Entities/SignedRequest.php');
 
 require_once( 'Facebook/FacebookSession.php' );
+require_once( 'Facebook/FacebookSignedRequestFromInputHelper.php');
 require_once( 'Facebook/FacebookCanvasLoginHelper.php');
 require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
 require_once( 'Facebook/FacebookRequest.php' );
@@ -18,11 +22,15 @@ require_once( 'Facebook/GraphObject.php' );
 require_once( 'Facebook/GraphUser.php');
 require_once( 'Facebook/GraphSessionInfo.php' );
  
-use Facebook\FacebookHttpable;
-use Facebook\FacebookCurl;
-use Facebook\FacebookCurlHttpClient;
+use Facebook\HttpClients\FacebookHttpable;
+use Facebook\HttpClients\FacebookCurl;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+
+use Facebook\Entities\AccessToken;
+use Facebook\Entities\SignedRequest;
 
 use Facebook\FacebookSession;
+use Facebook\FacebookSignedRequestFromInputHelper;
 use Facebook\FacebookCanvasLoginHelper;
 use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
